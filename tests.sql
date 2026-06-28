39dd6792b6941f2fed7fd74b86661f393aa347eb
Max <maksym.maksymenko.25@kse.org.ua>
2026-06-28 13:11:18 +0300

    added tests and everythign works ig

---
+++ tests.sql
@@ -0,0 +1,16 @@
+﻿insert into customers (full_name, email, balance)
+values ('Mister Biznes', 'email@email.com', 1234.56);
+
+insert into products (product_name, price, stock_quantity)
+values ('Banan', 500.00, 100);
+
+call create_order(1);
+
+call add_product_to_order(1, 1, 2);
+
+select order_id, total_amount from orders;
+
+select product_name, stock_quantity from products;
+
+select * from order_log;
+
